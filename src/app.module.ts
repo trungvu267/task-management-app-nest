08382f02e2e7a045d52299eb730a1725d521eb1b
VuTrung <vutrung26072001@gmail.com>
2023-08-04 23:37:18 +0700

    create module draft

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, TaskModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
